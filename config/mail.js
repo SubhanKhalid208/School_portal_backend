270b9e2f04ec0b17edf8288cf667aebbb2c73b13
subhankhalid208-rgb <subhankhalid208@gmail.com>
2026-02-09 16:48:48 +0500

    qdwede

--- config/mail.js
+++ config/mail.js
@@ -1,9 +1,23 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465, 
+  secure: true, 
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_PASS  
+  },
+  tls: {
+    rejectUnauthorized: false 
+  }
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("❌ Email Transporter Error:", error);
+  } else {
+    console.log("✅ Lahore Portal: Email server is ready to send messages!");
   }
 });
